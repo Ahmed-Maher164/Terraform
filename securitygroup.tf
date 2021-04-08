86099752b8cf8ed5085077fd82ff50a2774170e0
Ahmed-Maher164 <ahmed164maher@gmail.com>
2021-04-08 18:49:26 +0200

    update security group

--- securitygroup.tf
+++ securitygroup.tf
@@ -32,6 +32,13 @@ resource "aws_security_group" "allow_ssh_tcp" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   
 
   tags = {
