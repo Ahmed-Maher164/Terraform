d4dee637a302ebe80c3204146956097535eb8c85
Ahmed-Maher164 <ahmed164maher@gmail.com>
2021-04-08 16:48:03 +0200

    add rds and elastic cash and security group

--- securitygroup.tf
+++ securitygroup.tf
@@ -23,7 +23,7 @@ resource "aws_security_group" "allow_ssh_tcp" {
     from_port   = 3000
     to_port     = 3000
     protocol    = "tcp"
-    cidr_blocks = [module.network.vpc_cidr]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
@@ -32,8 +32,52 @@ resource "aws_security_group" "allow_ssh_tcp" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  
 
   tags = {
     Name = "allow_ssh_tcp"
   }
+}
+
+
+resource "aws_security_group" "rds" {
+  name = "rds"
+  vpc_id      = module.network.vpc_id
+
+    ingress {
+    description = "TLS from VPC"
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = [module.network.vpc_cider_block]
+  }
+  
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
+resource "aws_security_group" "redis" {
+  name = "redis"
+  vpc_id      = module.network.vpc_id
+
+    ingress {
+    description = "TLS from VPC"
+    from_port   = 6379
+    to_port     = 6379
+    protocol    = "tcp"
+    cidr_blocks = [module.network.vpc_cider_block]
+  }
+  
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
 }
